220be325699b7910f04cfc5595cd1c23eefc1c16
Danny Berrios <denny_berrios@hotmail.com>
2024-11-19 15:12:16 -0300

    Criando projeto base, Portfólio - Beyond the Code

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,86 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
-  plugins: [],
+	darkMode: ["class"],
+	content: [
+		"./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+		extend: {
+			colors: {
+				'bg-light': '#F5F5F5',
+				'text-dark': '#2F4F4F',
+				'bg-white': '#FFFFFF',
+				'text-indigo': '#4B0082',
+				'text-dark-grey': '#555555',
+				'bg-dodger-blue': '#1E90FF',
+				'text-white': '#FFFFFF',
+				'bg-indigo': '#4B0082',
+				'text-golden-yellow': '#F0E68C',
+				'bg-tomato-red': '#FF6347',
+				'bg-light-grey': '#F4F4F9',
+				'text-light-blue': '#3A3D98',
+				'bg-blog-light': '#F5F5F5',
+				'text-blog-blue': '#1E90FF',
+				'footer-bg': '#4B0082',
+				'footer-text': '#F5F5F5',
+				'footer-link': '#32CD32',
+				background: 'hsl(var(--background))',
+				foreground: 'hsl(var(--foreground))',
+				card: {
+					DEFAULT: 'hsl(var(--card))',
+					foreground: 'hsl(var(--card-foreground))'
+				},
+				popover: {
+					DEFAULT: 'hsl(var(--popover))',
+					foreground: 'hsl(var(--popover-foreground))'
+				},
+				primary: {
+					DEFAULT: 'hsl(var(--primary))',
+					foreground: 'hsl(var(--primary-foreground))'
+				},
+				secondary: {
+					DEFAULT: 'hsl(var(--secondary))',
+					foreground: 'hsl(var(--secondary-foreground))'
+				},
+				muted: {
+					DEFAULT: 'hsl(var(--muted))',
+					foreground: 'hsl(var(--muted-foreground))'
+				},
+				accent: {
+					DEFAULT: 'hsl(var(--accent))',
+					foreground: 'hsl(var(--accent-foreground))'
+				},
+				destructive: {
+					DEFAULT: 'hsl(var(--destructive))',
+					foreground: 'hsl(var(--destructive-foreground))'
+				},
+				border: 'hsl(var(--border))',
+				input: 'hsl(var(--input))',
+				ring: 'hsl(var(--ring))',
+				chart: {
+					'1': 'hsl(var(--chart-1))',
+					'2': 'hsl(var(--chart-2))',
+					'3': 'hsl(var(--chart-3))',
+					'4': 'hsl(var(--chart-4))',
+					'5': 'hsl(var(--chart-5))'
+				},
+			},
+			borderRadius: {
+				lg: 'var(--radius)',
+				md: 'calc(var(--radius) - 2px)',
+				sm: 'calc(var(--radius) - 4px)'
+			},
+			backgroundImage: {
+				'header-gradient': 'linear-gradient(to right, #1E90FF, #4B0082)',
+			},
+		}
+	},
+	plugins: [
+		// eslint-disable-next-line @typescript-eslint/no-require-imports
+		require("tailwindcss-animate")
+	],
 };
 export default config;
