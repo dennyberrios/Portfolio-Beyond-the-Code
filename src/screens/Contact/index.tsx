a01c68f054d826310d80b41afda8a8db235b7896
Danny Berrios <denny_berrios@hotmail.com>
2025-01-02 17:48:30 -0300

    Finalizando a responsividade

--- src/screens/Contact/index.tsx
+++ src/screens/Contact/index.tsx
@@ -10,7 +10,7 @@ const ContactPage = () => {
     return (
         <>
             <h1 className="lg:text-4xl max-lg:text-3xl max-sm:text-2xl font-black text-deep-indigo mt-20 pb-5">Entre em Contato</h1>
-            <p className="text-dark-grey lg:text-xl max-lg:text-lg max-sm:text-base mb-5">Estou sempre aberto a novas oportunidades e parcerias. Conecte-se comigo nas redes sociais ou envie uma mensagem.</p>
+            <p className="text-dark-grey lg:text-xl max-lg:text-lg max-sm:text-base mb-5 text-center px-2">Estou sempre aberto a novas oportunidades e parcerias. Conecte-se comigo nas redes sociais ou envie uma mensagem.</p>
             <section className="flex gap-5 py-10 mb-10 flex-wrap items-center justify-center">
                 <a href="https://www.instagram.com/berrios_danny30/" title="Instagram" target="_blank" className="flex items-center flex-col shadow-md p-3 hover:-translate-y-2 transform duration-200 ease-in-out w-40 bg-indigo-100 rounded-sm">
                     <FaInstagram size={50} fill="#E1306C"/>
@@ -35,8 +35,7 @@ const ContactPage = () => {
             </section>
             <section className="bg-dark-blue-gray w-full flex justify-center items-center flex-col">
                 <h2 className="text-light-bg lg:text-4xl max-lg:text-3xl max-sm:text-2xl font-black mt-20 pb-5">Fale Comigo</h2>
-                <p className="text-light-bg lg:text-xl max-lg:text-lg max-sm:text-base">Tem um projeto em mente ou quer apenas trocar uma ideia?</p>
-                <p className="text-light-bg lg:text-xl max-lg:text-lg max-sm:text-base">Preencha o formulário abaixo!</p>
+                <p className="text-light-bg lg:text-xl max-lg:text-lg max-sm:text-base text-center px-2">Tem um projeto em mente ou quer apenas trocar uma ideia? <br/> Preencha o formulário abaixo!</p>
                 <FormContact />
             </section>
         </>
