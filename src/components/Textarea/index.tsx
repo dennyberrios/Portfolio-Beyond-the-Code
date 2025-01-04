b943f6568a28f647800bff7e0aafb6ab63732d6e
Danny Berrios <denny_berrios@hotmail.com>
2025-01-03 23:35:30 -0300

    Adiciona o atributo 'name' ao textareaDemo.

--- src/components/Textarea/index.tsx
+++ src/components/Textarea/index.tsx
@@ -5,14 +5,20 @@ interface TTextareaDemo extends React.InputHTMLAttributes<HTMLTextAreaElement> {
   placeholder?: string
   label?: string
   colorLabel?: string
+  name?: string
   required?: boolean
 }
 
-export const TextareaDemo: React.FC<TTextareaDemo> = ({ placeholder, label, colorLabel, required, ...props }) => {
+export const TextareaDemo: React.FC<TTextareaDemo> = ({ placeholder, label, name, colorLabel, required, ...props }) => {
   return (
     <>
       <Label  className={`block text-[1.2rem] mb-2 mt-5 ${colorLabel}`}>{label}</Label>
-      <Textarea required={required} {...props} placeholder={placeholder} />
+      <Textarea 
+        required={required} 
+        {...props} 
+        placeholder={placeholder} 
+        name={name}
+      />
     </>
   )
 }
