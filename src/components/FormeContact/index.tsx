bc203a5b1cf0fa33a408973b251317c751e9cbb0
Danny Berrios <denny_berrios@hotmail.com>
2025-01-03 23:31:58 -0300

    Adiciona atributo 'name' nos inputs de formContact e implementa lógica de envio por email utilizando Formspree.
    Adiciona o atributo 'name' nos campos do formulário (name, email, message) para garantir a captura correta dos dados.
    Implementa a lógica de envio do formulário via Formspree, utilizando o endpoint de envio para enviar os dados do formulário para o email configurado.
    Configura a ação do formulário para utilizar o Formspree e o método POST para o envio.

--- src/components/FormeContact/index.tsx
+++ src/components/FormeContact/index.tsx
@@ -1,25 +1,61 @@
+'use client'
+
+import { useState } from "react";
 import { InputDemo } from "../Input";
 import { TextareaDemo } from "../Textarea";
 import { Button } from "../ui/button";
 
 const FormContact = () => {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const [status, setStatus] = useState<"idle" | "success" | "error">("idle")
+
+    async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+        e.preventDefault();
+
+        const form = e.currentTarget;
+        const formData = new FormData(form)
+
+        try {
+            const response = await fetch("https://formspree.io/f/xlddogag", {
+                method: "POST",
+                body: formData,
+                headers: {Accept: "application/json"},
+            });
+
+            if (response.ok) {
+                setStatus("success");
+                form.reset();
+            } else {
+                setStatus("error");
+            }
+        } catch (error) {
+            setStatus("error");
+            console.log(error);
+        }
+    }
+
     return (
-        <form className="lg:w-[600px] max-lg:w-[500px] max-sm:w-11/12 py-10">
-            <div>
+        <form
+            onSubmit={handleSubmit}
+            className="lg:w-[600px] max-lg:w-[500px] max-sm:w-11/12 py-10">
+               <div>
                 <InputDemo
                     placeholder="Seu nome"
                     type="text"
+                    name="name"
                     label="Nome"
                     colorLabel="text-light-bg"
                     className="bg-deep-slate border-none w-full py-6 px-3 text-light-grey text-lg focus:outline-none"
                     style={{fontSize: "1rem"}}
                     required={true}
                 />
+                
             </div>
             <div>
                 <InputDemo
                     placeholder="Seu E-email"
                     type="email"
+                    name="email"
                     label="E-mail"
                     colorLabel="text-light-bg"
                     className="bg-deep-slate border-none w-full py-6 px-3 text-light-grey"
@@ -30,6 +66,7 @@ const FormContact = () => {
             <div>
                 <TextareaDemo
                     placeholder="Sua mensagem aqui..."
+                    name="message"
                     label="Mensagem"
                     colorLabel="text-light-bg"
                     className="bg-deep-slate resize-none border-none w-full h-32 text-light-grey"
