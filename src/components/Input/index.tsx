2c2c8d1183bae97f31924b0e646f21fe25316039
Danny Berrios <denny_berrios@hotmail.com>
2025-01-03 23:34:13 -0300

    Adiciona o atributo 'name' ao inputDemo.

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -4,16 +4,23 @@ import { Label } from "../ui/label";
 interface IInputDemo extends React.InputHTMLAttributes<HTMLInputElement> {
   type?: string
   placeholder?: string
+  name?: string
   label?: string
   colorLabel?: string
   required?: boolean
 }
 
-export const InputDemo: React.FC<IInputDemo> = ({ type, placeholder, label, colorLabel, required, ...props }) => {
+export const InputDemo: React.FC<IInputDemo> = ({ type, placeholder, name, label, colorLabel, required, ...props }) => {
   return (
     <>
       <Label className={`block text-[1.2rem] mb-2 mt-5 ${colorLabel}`}>{label}</Label>
-      <Input {...props} required={required} type={type} placeholder={placeholder} />
+      <Input 
+        {...props} 
+        required={required} 
+        type={type} 
+        placeholder={placeholder} 
+        name={name}
+      />
     </>
   )
 }
