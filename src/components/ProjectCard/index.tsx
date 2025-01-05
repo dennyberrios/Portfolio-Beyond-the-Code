7a6de0c85e92f3cbb18d83e003bb51d9c5e2b722
Danny Berrios <denny_berrios@hotmail.com>
2025-01-04 21:55:05 -0300

    Refatoração do código de ProjectCard"
    Explicação:
    Este commit refatora o código do componente ProjectCard, melhorando sua estrutura, clareza ou performance. A refatoração pode envolver simplificação de lógica, reorganização do código ou aplicação de boas práticas, tornando o componente mais eficiente e fácil de manter

--- src/components/ProjectCard/index.tsx
+++ src/components/ProjectCard/index.tsx
@@ -3,21 +3,33 @@ import { Button } from "../ui/button";
 import Image, { StaticImageData } from "next/image";
 
 interface PProjectCard {
-    img?: StaticImageData;
+    img?: StaticImageData | string;
     alt?: string;
     title?: string;
     description?: string;
     link?: string;
 }
 
-const ProjectCard: React.FC<PProjectCard> = ({ img, alt, title, description, link }) => {
+const ProjectCard: React.FC<PProjectCard> = ({
+    img,
+    alt = "Gif do Projeto em Exibição",
+    title = "Título do Projeto",
+    description = "Descrição do projeto",
+    link = "/",
+}) => {
     return (
         <div className="w-[310px] flex flex-col justify-center items-center rounded-xl bg-deep-indigo hover:shadow-lg mt-5 hover:-translate-y-4 transition duration-300 ease-in-out">
-            {img && (<Image src={img} alt={alt || "Giff do Projeto em Exibição"} className="w-4/5 rounded-md -mt-5 shadow-md"/>)}
+            {img && (<Image
+                src={img}
+                alt={alt || "Giff do Projeto em Exibição"}
+                className="w-4/5 rounded-md -mt-5 shadow-md"
+                width={"400"}
+                height={300}
+            />)}
             <h3 className="text-white mt-7 mb-3 text-2xl font-semibold">{title}</h3>
             <p className="text-golden-yellow">{description}</p>
             <Button asChild className="hover:bg-golden-yellow bg-tomato-red mt-6 mb-5 w-8/12 transition duration-200 ease-in-out">
-                <Link href={link || "/"} target="_blank" className="hover:text-deep-indigo text-lg font-semibold transition duration-200 ease-in-out">Ver Mais</Link>
+                <Link href={link || "/"} className="hover:text-deep-indigo text-lg font-semibold transition duration-200 ease-in-out">Ver Mais</Link>
             </Button>
         </div>
     );
